82fa4b2b2384d119b9457acac769aba90f99c02b
Jnimatuj <jose.nimatuj02@gmail.com>
2025-05-27 19:30:53 -0600

    cargo cosas

--- ProyectoFinalEstadisticaII/Form1.cs
+++ ProyectoFinalEstadisticaII/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace ProyectoFinalEstadisticaII
@@ -15,6 +9,98 @@ namespace ProyectoFinalEstadisticaII
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private bool revEnter(KeyPressEventArgs e) => e.KeyChar == (char)Keys.Enter;
+
+        private bool revRegex(string text) => Regex.IsMatch(text, @"^\d+$");
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (revEnter(e))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (revEnter(e) && revRegex(textBox2.Text))
+            {
+                listBox2.Items.Add(textBox2.Text);
+                textBox2.Clear();
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (revEnter(e) && revRegex(textBox3.Text))
+            {
+                listBox3.Items.Add(textBox3.Text);
+                textBox3.Clear();
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (revEnter(e) && revRegex(textBox4.Text))
+            {
+                listBox4.Items.Add(textBox4.Text);
+                textBox4.Clear();
+            }
+        }
+
+        private bool revListas()
+        {
+            int a = listBox2.Items.Count,
+                b = listBox3.Items.Count,
+                c = listBox4.Items.Count;
+
+            if (a > 2)
+            {
+                MessageBox.Show("Ingresar solo 2 grupos porfis");
+                return false;
+            }
+
+            if (a == b && b == c && a == 2 && a > 0)
+            {
+                return true;
+            }
+
+            MessageBox.Show("Verifica que todas las listas tengan 2 valores.");
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!revListas()) return;
+
+            try
+            {
+                float[] pro = {
+                    float.Parse(listBox2.Items[0].ToString()),
+                    float.Parse(listBox2.Items[1].ToString())
+                };
+                float[] var = {
+                    float.Parse(listBox3.Items[0].ToString()),
+                    float.Parse(listBox3.Items[1].ToString())
+                };
+                int[] con = {
+                    int.Parse(listBox4.Items[0].ToString()),
+                    int.Parse(listBox4.Items[1].ToString())
+                };
+
+                PruebaT prueba = new PruebaT(pro, var, con);
+
+                string resultado = prueba.ObtenerInterpretacion();
+
+                MessageBox.Show(resultado, "Resultado Prueba t");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
---
+++ ProyectoFinalEstadisticaII/PruebaT.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ProyectoFinalEstadisticaII
+{
+    public class PruebaT
+    {
+        public float[] Promedios { get; set; } = new float[2];
+        public float[] Varianzas { get; set; } = new float[2];
+        public int[] Muestras { get; set; } = new int[2];
+
+        public int GradosLibertad => Muestras[0] + Muestras[1] - 2;
+
+        public float ValorCriticoT { get; set; } = 2.021f; // Para alfa=0.05 y gl=40 por defecto
+        public float Alfa { get; set; } = 0.05f;
+
+        public PruebaT(float[] promedios, float[] varianzas, int[] muestras)
+        {
+            if (promedios.Length != 2 || varianzas.Length != 2 || muestras.Length != 2)
+                throw new ArgumentException("Los arreglos deben tener exactamente 2 elementos.");
+
+            Promedios = promedios;
+            Varianzas = varianzas;
+            Muestras = muestras;
+        }
+
+        public float CalcularVarianzaAgrupada()
+        {
+            int n1 = Muestras[0];
+            int n2 = Muestras[1];
+
+            float Sp2 = (((n1 - 1) * Varianzas[0]) + ((n2 - 1) * Varianzas[1])) / (n1 + n2 - 2);
+            return Sp2;
+        }
+
+        public float CalcularEstadisticoT()
+        {
+            int n1 = Muestras[0];
+            int n2 = Muestras[1];
+
+            float Sp2 = CalcularVarianzaAgrupada();
+            if (Sp2 < 0) throw new Exception("Varianza agrupada negativa, datos inválidos.");
+
+            float numerador = Promedios[0] - Promedios[1];
+            float denominador = (float)(Math.Sqrt(Sp2) * Math.Sqrt((1.0 / n1) + (1.0 / n2)));
+
+            if (denominador == 0)
+                throw new DivideByZeroException("Denominador cero en cálculo de estadístico t.");
+
+            return numerador / denominador;
+        }
+
+        public string ObtenerInterpretacion()
+        {
+            float tCalc = CalcularEstadisticoT();
+            int gl = GradosLibertad;
+
+            if (Math.Abs(tCalc) > ValorCriticoT)
+            {
+                return $"t calculado = {tCalc:F2} > t crítico = {ValorCriticoT}, " +
+                       $"con {gl} grados de libertad y alfa = {Alfa}, se RECHAZA la hipótesis nula.\n" +
+                       "Hay diferencia significativa entre los grupos.";
+            }
+            else
+            {
+                return $"t calculado = {tCalc:F2} ≤ t crítico = {ValorCriticoT}, " +
+                       $"con {gl} grados de libertad y alfa = {Alfa}, NO se rechaza la hipótesis nula.\n" +
+                       "No hay diferencia significativa entre los grupos.";
+            }
+        }
+    }
+}
